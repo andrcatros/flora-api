bb5cecbd4457aac11a8d7596483c8f67e736ff46
Andreea Ros <ros.andreeacatalina@gmail.com>
2020-08-30 17:25:06 +0100

    Set up basic POST and GET requests.

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,53 @@
+//import libraries
 const functions = require("firebase-functions");
-
 const admin = require("firebase-admin");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-admin.initializeApp();
+//initialize firebase in order to access its services
+admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
+//initialize express server
+const app = express();
+const main = express();
+
+//add the path to receive request and set json as bodyParser to process the body
+main.use("/api/v1", app);
+main.use(bodyParser.json());
+main.use(bodyParser.urlencoded({ extended: false }));
+
+//initialize the database and the collection
+const db = admin.firestore();
+const productCollection = "products";
+
+//define google cloud function name
+exports.webApi = functions.https.onRequest(main);
+
+// post to products collection
+app.post("/all-products", async (req, res) => {
+  try {
+    const product = {
+      name: req.body["name"],
+      price: req.body["price"],
+      description: req.body["description"],
+      category: req.body["category"],
+      id: req.body["id"],
+    };
+
+    const newDoc = await db.collection(productCollection).add(product);
+    res.status(201).send(`Created new product: ${newDoc.id}`);
+  } catch (error) {
+    res.status(400).send(`${error}`);
+  }
 });
 
-exports.hello = functions.https.onRequest((req, res) => {
-  functions.logger.info("I am logging");
-  res.send("Yes, this worked!");
+// get all products
+app.get("/all-products", async (req, res) => {
+  try {
+    const snapshot = await db.collection(productCollection).get();
+    const resDocs = snapshot.docs.map((doc) => doc.data());
+    res.send(resDocs);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
